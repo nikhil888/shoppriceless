b038fb037915d3ed306369fbc9ad24b668539333
unknown <nikkhil888@gmail.com>
2021-01-23 01:46:35 +0530

    Prepare for deployment

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import multer from 'multer'
+import pah from 'path';
+
+const router = express.Router()
+
+const storage = multer.diskStorage({
+    destination(req,file,db){
+        cb(null,'uploads/')
+    },
+    filename(req,file,cb){
+        cb(null,`${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+function checkFileType(file,cb){
+    const filetypes = /jpeg|jpg|png/
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+    const mimetype = filetypes.test(file.mimetype)
+
+    if(extname && mimetype){
+        return console(null,true)
+    }else{
+        cb('Images only!')
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter:function(req,file,cb){
+        checkFileType(file,cb)
+    }
+})
+
+
+router.post('/',upload.single('image'),(req,res) => {
+    res.send(`/${req.file.path}`)
+})
+
+export default router
